751fb3324f10a607277adbc5226d041601b34fdd
zxk <919792828@qq.com>
2018-05-31 16:21:50 +0800

    iconfont

---
+++ README.md
@@ -0,0 +1,26 @@
+﻿# iconfont
+
+SVG生成字体包，并生成css 样式引用
+
+## 技术栈
+
+gulp-iconfont + gulp-iconfont-css + gulp-template
+
+
+## 项目运行 运行目录  /view/coreIcon/ 
+安装node
+npm install 
+gulp 
+
+## 项目使用
+将新的svg图片放入src/core/icons/ 下
+命令行输入gulp  
+等待gulp执行完毕
+在build/core/下
+css目录下是我们所需的iconfont.css 文件，
+fonts是我们的字体文件，将字体文件和css文件放入core/view 下 注意路径
+
+只需要将这个iconfont.css引入页面即可
+example目录下的index是图标示例，可以来这里看图标对应的class
+
+祝你工作愉快  (*@ο@*) 哇～
---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+var gulp = require('gulp');
+var iconfont = require('gulp-iconfont');
+var iconfontCss = require('gulp-iconfont-css');
+var clean = require("gulp-clean");
+var taskListing = require("gulp-task-listing");
+var template = require("gulp-template");
+var rename = require('gulp-rename');
+
+var del = require("del");
+
+var fs = require("fs");
+var util = require("./en-cn.js");
+
+// 发布项目
+var filt = ["core"],
+    task = {
+        // 发布打包
+        iconfont: function(name) {
+            return gulp.src('./src/' + name + '/icons/*.svg')
+                .pipe(rename(function(path) {
+                    path.basename = util.getPym(path.basename, true);
+                }))
+                .pipe(iconfontCss({
+                    fontName: fontName,
+                    path: './src/templates/iconfont.css',
+                    targetPath: '../css/iconfont.css',
+                    fontPath: '../fonts/'
+                }))
+                .pipe(iconfont({
+                    fontName: fontName,
+                    formats: ['svg', 'ttf', 'eot', 'woff', 'woff2'],
+                    normalize: true,
+                    options: {
+                        fixedWidth: false,
+                        normalize: false,
+                        fontHeight: 512,
+                        descent: -32,
+                        normalize:true
+                      }
+                }))
+                .on('glyphs', function(glyphs, options) {
+                    // CSS templating, e.g.           
+                    //console.log(glyphs, options);
+                })
+                .pipe(gulp.dest('./build/' + name + '/fonts/'));
+        },
+        example: function(name) {
+
+            //console.log(this.icons(name));
+            return gulp.src('./src/example/index.html')
+                .pipe(template({
+                    icons: this.icons(name)
+                }))
+                .pipe(gulp.dest("./build/" + name + "/example"));
+        },
+        clean: function(name) {
+            var icons = this.icons(name);
+            return gulp.src("./build/" + name, {
+                read: false
+            }).pipe(clean());
+        },
+        icons: function(name) {
+            var icons = fs.readdirSync("./src/" + name + "/icons/");
+            icons = icons.map(function(icon) {
+                return util.getPym(icon.replace(/\.\w+$/, ''), true);
+            });
+            return icons;
+        }
+    };
+
+
+
+var fontName = 'iconfont';
+gulp.task("del", function() {
+        return del(['./build/*']);
+})
+gulp.task('default', ["del"], function() {
+    for (var key in task) {
+        if (key != "icons") {
+            for (var key2 in filt) {
+                task[key](filt[key2]);
+            }
+        }
+    }
+});
+
+
